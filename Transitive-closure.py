006e373e46442751ae24dde0123d6e3dbbe9061f
samuelmayna <samuelmayna@gmail.com>
2022-08-07 12:57:17 +0300

    Able to read the edges from the array and display them as using the networkx package

---
+++ Transitive-closure.py
@@ -0,0 +1,69 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def draw_initial_graphs(graphs):
+    for graph in graphs:
+        key = list(graph.keys())[0]
+        edges = graph[key]
+        G = nx.DiGraph()
+        G.add_edges_from(edges)
+        pos = nx.spring_layout(G)
+        nx.draw_networkx_nodes(G, pos, node_size=500)
+        nx.draw_networkx_edges(G, pos,
+                               edgelist=G.edges(data=True), edge_color='black')
+        nx.draw_networkx_labels(G, pos)
+        plt.title(key)
+        plt.savefig(key + '.png')
+        plt.show()
+
+
+# different types of graphs which will produce very different transtive closures.
+equivalence_relation = {"equivalnce_relation": [(1, 2),
+                                                (2, 3),
+                                                (3, 1),
+                                                (4, 5),
+                                                (5, 6),
+                                                (6, 4)]
+                        }
+
+finer_equivalence = {"finer_equivalence": [(1, 2),
+                                           (2, 1),
+                                           (3, 4),
+                                           (4, 3),
+                                           (5, 6),
+                                           (6, 6)]}
+
+
+total_ordering = {"total_ordering": [(1, 2),
+                                     (2, 3),
+                                     (3, 4),
+                                     (4, 5),
+                                     (5, 6),
+                                     (6, 7),
+                                     (7, 8),
+                                     ]}
+
+partial_ordering = {"partial_ordering": [(1, 2),
+                                         (2, 3),
+                                         (3, 4),
+                                         (5, 6),
+                                         (6, 7),
+                                         (7, 8),
+                                         ]}
+universal_relation = {"universal_relation": [
+    (1, 2),
+    (2, 3),
+    (3, 4),
+    (4, 5),
+    (5, 6),
+    (6, 7),
+    (7, 8),
+    (8, 1),
+]}
+
+
+if __name__ == "__main__":
+    graphs = [equivalence_relation, finer_equivalence, universal_relation,
+              total_ordering, partial_ordering, universal_relation]
+    draw_initial_graphs(graphs=graphs)
